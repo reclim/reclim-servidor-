b8f4738a6ccc9fe611c90b24eacafb6109758af2
reclim <fredjbps@gmail.com>
2026-08-18 10:46:18 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "reclim-licenca-cache-v1";
+const ARQUIVOS = [
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-192-maskable.png",
+  "./icon-512-maskable.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ARQUIVOS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((chaves) =>
+      Promise.all(
+        chaves
+          .filter((chave) => chave !== CACHE_NAME)
+          .map((chave) => caches.delete(chave))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resposta) => {
+      return resposta || fetch(event.request);
+    })
+  );
+});
